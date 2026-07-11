6a84f03950a05ae6671a7597662d5209e98b0a77
Prisha Verma <vprisha94@gmail.com>
2026-07-12 00:28:10 +0530

    Add C++ learning notes from tut30 to tut35.cpp

---
+++ tut35.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+// Destructor never takes an argument nor does it return any value
+
+int count = 0;
+class num{
+    public:
+       num(){
+        count++;
+        cout<<"this is the time when constructor is called for object number "<<count<<endl;
+       }
+
+       ~num(){
+           cout<<"this is the time when my destructor is called for object number "<<count<<endl;
+           count--;
+       }
+};
+
+int main(){
+    cout<<"we are inside a main function "<<endl;
+    cout<<"creating first object n1 "<<endl;
+    num n1;
+    {
+        cout<<"entering this block "<<endl;
+        cout<<" creating two more objects "<<endl;
+        num n2, n3;
+        cout<<"exiting this block "<<endl;
+    }
+    cout<<"back to main "<<endl;
+    return 0;
+}
